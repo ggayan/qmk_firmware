10158d7783c71a051f35084a4f929e8f61fa814c
Gabriel Gayan <gabrielgayan@gmail.com>
2019-05-24 19:22:51 -0700

    Fix hold ctrl

--- keyboards/ergodox_ez/keymaps/ggayan/keymap.c
+++ keyboards/ergodox_ez/keymaps/ggayan/keymap.c
@@ -23,7 +23,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     // right hand
     KC_EQUAL,    KC_6,   KC_7,  KC_8,   KC_9,     KC_0,          KC_MINUS,
     KC_BSPACE,   KC_Y,   KC_U,  KC_I,   KC_O,     KC_P,          KC_BSLASH,
-                 KC_H,   KC_J,  KC_K,   KC_L,     KC_SCOLON,     RCTL(KC_QUOTE),
+                 KC_H,   KC_J,  KC_K,   KC_L,     KC_SCOLON,     RCTL_T(KC_QUOTE),
     KC_ESCAPE,   KC_N,   KC_M,  KC_COMM,KC_DOT,   KC_SLASH,      KC_RSFT,
                         KC_UP,  KC_DOWN,KC_LBRC,  KC_RBRC, KC_RCTRL,
 
