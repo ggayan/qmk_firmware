40db482994c6edc624c272d5812d059e62c45c91
Gabriel Gayan <gabrielgayan@gmail.com>
2019-05-24 18:58:51 -0700

    use one-shot key for layer change

--- keyboards/ergodox_ez/keymaps/ggayan/keymap.c
+++ keyboards/ergodox_ez/keymaps/ggayan/keymap.c
@@ -14,7 +14,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TAB,        KC_Q,     KC_W,     KC_E,      KC_R,  KC_T,   KC_TAB,
     KC_LGUI,       KC_A,     KC_S,     KC_D,      KC_F,  KC_G,
     KC_LSFT,       KC_Z,     KC_X,     KC_C,      KC_V,  KC_B,   KC_DEL,
-    MO(L_SYMB),    KC_LCTL,  KC_LALT,  KC_LEFT,   KC_RIGHT,
+    OSL(L_SYMB),   KC_LCTL,  KC_LALT,  KC_LEFT,   KC_RIGHT,
 
                                                  LCTL(KC_C), LCTL(KC_V),
                                                                 KC_LALT,
@@ -23,13 +23,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     // right hand
     KC_EQUAL,    KC_6,   KC_7,  KC_8,   KC_9,     KC_0,          KC_MINUS,
     KC_BSPACE,   KC_Y,   KC_U,  KC_I,   KC_O,     KC_P,          KC_BSLASH,
-                 KC_H,   KC_J,  KC_K,   KC_L,     KC_SCOLON,     KC_QUOTE,
+                 KC_H,   KC_J,  KC_K,   KC_L,     KC_SCOLON,     RCTL(KC_QUOTE),
     KC_ESCAPE,   KC_N,   KC_M,  KC_COMM,KC_DOT,   KC_SLASH,      KC_RSFT,
                         KC_UP,  KC_DOWN,KC_LBRC,  KC_RBRC, KC_RCTRL,
 
     KC_PGUP, KC_PGDN,
-    KC_RALT,
-    KC_RCTL, MO(L_SYMB), KC_ENTER
+    TG(L_HOTS),
+    KC_RALT, MO(L_SYMB), KC_ENTER
 ),
 [L_OSX] = LAYOUT_ergodox(  // layer 0 : default
     // left hand
@@ -37,7 +37,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TAB,         KC_Q,     KC_W,      KC_E,      KC_R,   KC_T,   KC_TAB,
     KC_HYPR,        KC_A,     KC_S,      KC_D,      KC_F,   KC_G,
     KC_LSFT,        KC_Z,     KC_X,      KC_C,      KC_V,   KC_B,   KC_DELETE,
-    MO(L_SYMB),     KC_LCTL,  KC_LALT,   KC_LEFT,   KC_RIGHT,
+    OSL(L_SYMB),    KC_LCTL,  KC_LALT,   KC_LEFT,   KC_RIGHT,
 
                                                       LGUI(KC_C),LGUI(KC_V),
                                                               LGUI(KC_LALT),
