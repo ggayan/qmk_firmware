a4f8b393cde70a0348a8da11f294fbd6ef025c39
Gabriel Gayan <gabrielgayan@fb.com>
2020-02-21 15:11:24 -0800

    trying different modifiers

--- keyboards/ergodox_ez/keymaps/ggayan/keymap.c
+++ keyboards/ergodox_ez/keymaps/ggayan/keymap.c
@@ -33,23 +33,23 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_ESCAPE,      KC_1,     KC_2,      KC_3,      KC_4,   KC_5,   KC_GRAVE,
     KC_TAB,         KC_Q,     KC_W,      KC_E,      KC_R,   KC_T,   KC_TAB,
     KC_HYPR,        KC_A,     KC_S,      KC_D,      KC_F,   KC_G,
-    KC_LSPO,        KC_Z,     KC_X,      KC_C,      KC_V,   KC_B,   KC_DELETE,
+    KC_LSFT,        KC_Z,     KC_X,      KC_C,      KC_V,   KC_B,   KC_DELETE,
     OSL(L_SYMB),    KC_LCTL,  KC_LALT,   KC_LEFT,   KC_RIGHT,
 
-                                                      LGUI(KC_C),LGUI(KC_V),
-                                                              LGUI(KC_LALT),
+                                                    LGUI(KC_TAB),   KC_LCTL,
+                                                                    KC_LALT,
                                                  KC_SPACE,KC_BSPACE,KC_LGUI,
 
     // right hand
     KC_EQUAL,     KC_6,   KC_7,  KC_8,   KC_9,     KC_0,          KC_MINUS,
     KC_LEAD,      KC_Y,   KC_U,  KC_I,   KC_O,     KC_P,          KC_BSLASH,
                   KC_H,   KC_J,  KC_K,   KC_L,     KC_SCOLON,     GUI_T(KC_QUOT),
-    KC_ESCAPE,    KC_N,   KC_M,  KC_COMM,KC_DOT,   KC_SLASH,      KC_RSPC,
+    KC_ESCAPE,    KC_N,   KC_M,  KC_COMM,KC_DOT,   KC_SLASH,      KC_RSFT,
                           KC_UP,  KC_DOWN,KC_LBRC,  KC_RBRC, KC_RCTRL,
 
-    KC_PGUP, KC_PGDN,
-    TG(L_HOTS),
-    KC_RALT, MO(L_SYMB), KC_ENTER
+    KC_RCTL, KC_PGDN,
+    KC_RALT,
+    KC_RGUI, MO(L_SYMB), KC_ENTER
 ),
 [L_SYMB] = LAYOUT_ergodox(
     // left hand
@@ -66,7 +66,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     RESET,    KC_F6,    KC_F7,    KC_F8,    KC_F9,  KC_F10,   KC_F11,
     KC_MAKE,  KC_UP,    KC_7,     KC_8,     KC_9,   KC_ASTR,  KC_F12,
               KC_DOWN,  KC_4,     KC_5,     KC_6,   KC_PLUS,  KC_L_OSX,
-    VRSN,     KC_AMPR,  KC_1,     KC_2,     KC_3,   KC_BSLS,  KC_L_WIN,
+ TG(L_HOTS),  KC_AMPR,  KC_1,     KC_2,     KC_3,   KC_BSLS,  KC_L_WIN,
                         _____,    KC_DOT,   KC_0,   KC_EQL,   _____,
 
     _____, _____,
